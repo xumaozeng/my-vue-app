352bd7ca874ede93fb07e7f0fed0d95de6f40875
xumaozeng <15611895341@163.com>
2020-12-22 23:30:14 +0800

    create my-vue-app

---
+++ Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>姓名：{{ name }}</p>
+    <span>年龄：{{ age }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "xiaoxu",
+      age: 25,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
